6c962b35e6cfcaaac4660973b9e85d2d8f7603b2
Max Chiu <mc4487@drexel.edu>
2024-02-11 13:03:40 -0500

    organized tutorials into one directory to not crowd main repository

---
+++ tutorial/gamev2tutorial/settings.py
@@ -0,0 +1,37 @@
+WIDTH = 1280
+HEIGHT = 720
+TILESIZE = 40
+
+GREEN = (20, 145, 54)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+
+FPS = 60
+
+PLAYER_LAYER = 1
+PLAYER_SPEED = 6
+BLOCK_LAYER = 2
+GROUND_LAYER = 3
+
+#tiles to represent the world, each 'B' represents a block, the 'P' is the player's position
+#There are 32 horizontal tiles (width of 1280 / 40) and 18 vertical tiles (height of 720 / 40))
+tilemap = [
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB',
+    'B..............................B',
+    'B..........BBBB................B',
+    'B.........BB..BB...............B',
+    'B.........BB..BB...............B',
+    'B..............................B',
+    'B..................B.B.........B',
+    'B..............................B',
+    'B.....BBBBBBB..................B',
+    'B...............P..............B',
+    'B..............................B',
+    'B..............................B',
+    'B..............................B',
+    'B..............................B',
+    'B..............................B',
+    'B..............................B',
+    'B..............................B',
+    'BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB'
+]
